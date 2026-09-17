e0311cc4dba05456ee7957b9d61c0f67b4b598d3
BenGoudas <benjaming185@nycstudents.net>
2026-09-17 15:34:25 -0400

    Added Welcome Message

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,9 +1,17 @@
 import java.util.Scanner;
+import java.io.File;
+import java.io.FileNotFoundException;
 public class HelloWorld {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {    
         Scanner s = new Scanner(System.in);
         System.out.println("What is your name?");
         String name = s.nextLine();
         System.out.println("Hello, " + name + "!");
+        File f = new File("Class.txt");
+        s.close();
+        s= new Scanner(f);
+        String course = s.nextLine();
+        System.out.println("Welcome to " + course + "!");
+        s.close();
     }    
 }
